f1b87a41030f2509e1185f73b666445fa7360a80
Tom Sawyer <tomsawyer@TomSawyer.local>
2018-04-05 16:20:05 +0700

    Node server is single thread, will block the Event Loop

--- Event_Loop/index.js
+++ Event_Loop/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 
+// Node Server is Single Thread -- Blocking the Event Loop
+// while run 'doWork' function, server do nothing until the first request is handle
+function doWork(duration) {
+  const start = Date.now();
+  while (Date.now() - start < duration) {}
+}
+
 app.get('/', (req, res) => {
+  doWork(5000);
   res.send('Hi there');
 });
 
