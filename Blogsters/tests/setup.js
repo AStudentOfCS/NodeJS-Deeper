4dab3325e3177c436487154adf3bd47c39c65a1c
Tom Sawyer <tomsawyer@TomSawyer.local>
2018-04-16 18:28:39 +0700

    Testing pipeline with Jest on Chromium using Puppeteer; Testing Header and Authentication Flow, combining Object Property Access using Proxy to mofidy Page implementation in Puppeteer

---
+++ Blogsters/tests/setup.js
@@ -0,0 +1,8 @@
+// Global Jest setup to connect mongoDB for testing environment
+require('../models/User');
+
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI, { useMongoClient: true });
